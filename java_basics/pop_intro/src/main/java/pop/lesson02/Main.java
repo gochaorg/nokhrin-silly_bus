6d4db436844fcd869912fc0af9540e4c3c83ed72
Aleksandr Nokhrin <a.v.nokhrin@yandex.ru>
2025-09-02 18:12:12 +0500

    - реализованы методы для задачи 2

--- java_basics/pop_intro/src/main/java/pop/lesson02/Main.java
+++ java_basics/pop_intro/src/main/java/pop/lesson02/Main.java
@@ -1,5 +1,7 @@
 package pop.lesson02;
 
+import java.math.BigInteger;
+
 /**
  * 1. Реализуйте `sum(int a, int b)` и `isEven(int n)`.
  * 2. Напишите итеративный `factorial(int n)`.
@@ -12,7 +14,7 @@ public class Main {
      * @return - целое число, сумма a и b
      */
     public int sum(int a, int b) {
-        return 0;
+        return a + b;
     }
 
     /**
@@ -21,16 +23,38 @@ public class Main {
      * @return - true для четного, false для нечетного
      */
     public boolean isEven(int n) {
-        return false;
+        return n % 2 == 0;
     }
 
     /**
-     * Возвращает факториал числа, итеративный алгоритм
-     * @param n - целое число
+     * Возвращает факториал числа, итеративный алгоритм, цикл for
+     * @param n - целое число, n <= 20 - максимум для итога типа long
      * @return - целое число - факториал n
+     * задача не говорит требования к типу результата, возможны варианты - long и BigInteger
+     * вариант с BigInteger определим как ответ на задание
      */
-    public long factorial_iterative(int n) {
-        return 0;
+    public BigInteger factorial(int n) {
+        BigInteger fact = BigInteger.valueOf(1);
+        for (int i = 1; i <= n; i++) {
+            fact = fact.multiply(BigInteger.valueOf(i));
+        }
+        return fact;
+    }
+
+    public long factorialIterativeForLoopLong(int n) {
+        long fact = 1;
+        for (int i = 1; i <= n; i++) {
+            fact *= i;
+        }
+        return fact;
+    }
+
+    public int factorialIterativeForLoopInt(int n) {
+        int fact = 1;
+        for (int i = 1; i <= n; i++) {
+            fact *= i;
+        }
+        return fact;
     }
 
     /**
@@ -38,11 +62,10 @@ public class Main {
      * @param n - целое число
      * @return - целое число - факториал n
      */
-    public long factorial_recursive(int n) {
-        return 0;
-    }
-
-    public static void main(String[] args) {
-        // todo
+    public long factorialRecursiveLong(int n) {
+        if (n == 1) {
+            return 1;
+        }
+        return factorialRecursiveLong(n - 1) * n;
     }
 }
